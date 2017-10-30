e679ecd647e355dcacdc3f03313bb2eb4bc63080
aruh <alexander.b.ruhland@gmail.com>
2017-10-30 14:11:14 +0100

    Initialize properties to undefined or default value.
    Initialization to undefined is required by json2typescript. See
    documentation here:
    https://github.com/dhlab-basel/json2typescript#mapping-example

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngularJSON2TypeScriptClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngularJSON2TypeScriptClientCodegen.java
@@ -8,7 +8,10 @@ import java.util.Map;
 
 public class TypeScriptAngularJSON2TypeScriptClientCodegen extends TypeScriptAngularClientCodegen {
 
-    private static final String KEY_J2T_PROPERTY_TYPE = "j2tPropertyType";
+    private static final String KEY_J2T_DATA_TYPE = "j2tDataType";
+    private static final String KEY_J2T_DEFAULT_VALUE = "j2tDefaultValue";
+    private static final String VALUE_NULL = "null";
+    private static final String VALUE_UNDEFINED = "undefined";
 
     public TypeScriptAngularJSON2TypeScriptClientCodegen() {
         super();
@@ -29,10 +32,12 @@ public class TypeScriptAngularJSON2TypeScriptClientCodegen extends TypeScriptAng
             for (Map<String, Object> model : models) {
                 List<CodegenProperty> properties = ((CodegenModel)model.get("model")).vars;
                 for (CodegenProperty property : properties) {
-                    property.vendorExtensions.put(KEY_J2T_PROPERTY_TYPE, buildJ2TPropertyType(property));
+                    property.vendorExtensions.put(KEY_J2T_DATA_TYPE, buildJ2TPropertyType(property));
                     if (property.isEnum) {
-                        property.vendorExtensions.put(KEY_J2T_PROPERTY_TYPE, property.datatypeWithEnum);
+                        property.vendorExtensions.put(KEY_J2T_DATA_TYPE, property.datatypeWithEnum);
                     }
+
+                    property.vendorExtensions.put(KEY_J2T_DEFAULT_VALUE, buildJ2TPropertyDefaultValue(property));
                 }
             }
 
@@ -50,6 +55,14 @@ public class TypeScriptAngularJSON2TypeScriptClientCodegen extends TypeScriptAng
     public String getHelp() {
         return "Generates a TypeScript Angular (2.x or 4.x) client library. " +
                 "Models are annotated to be deserialized with json2typescript.";
+    }
+
+    private String buildJ2TPropertyDefaultValue(CodegenProperty prop) {
+        if (prop.defaultValue.equals(VALUE_NULL)) {
+            return VALUE_UNDEFINED;
+        }
+
+        return prop.defaultValue;
     }
 
     private String buildJ2TPropertyType(CodegenProperty prop) {
